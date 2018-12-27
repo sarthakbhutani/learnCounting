6be4c0dd768a3da4826503b458f1090e7a7f344b
Sarthak <sarthakbhutani.work@gmail.com>
2018-12-28 01:43:10 +0530

    Proper Indentation + cleaning waste comments

--- backend/learnCounting_proto.js
+++ backend/learnCounting_proto.js
@@ -11,45 +11,28 @@ const GetNewFactHandler = {
         && request.intent.name === 'StartCountIntent');
   },
   handle(handlerInput) {
-    // const factArr = data;
-    // const factIndex = Math.floor(Math.random() * factArr.length);
-    // const randomFact = factArr[factIndex];
     var countnumber = handlerInput.requestEnvelope.request.intent.slots.count.value;
-    
-    //set Difficulty level
+    //set Difficulty level:
     var difficultyLevel = handlerInput.requestEnvelope.request.intent.slots.difficultyLevel.value;
-    
-    var count = ' ' + ' ' + ' ';  //change here
-  var speakOutput = "So, I am gonna begin the count "; //+ count + "<break time = \"4s\"/>" + "hey"
+    var count = ' ' + ' ' + ' ';  //CHANGEREQD
+    var speakOutput = "So, I am gonna begin the count "; //+ count + "<break time = \"4s\"/>" + "hey"
     for(var i = 1; i <= countnumber;i++){
-    // for(var i = 1; i <= 12;i++){
-          // count = count + String(i) + 'n' ;
-          if (difficultyLevel == 'high' )
+    if (difficultyLevel == 'high' )
       speakOutput = speakOutput + String(i) + " <break time = \"1s\"/>" ;
-      
       else if (difficultyLevel == 'mid' )
       speakOutput = speakOutput + String(i) + " <break time = \"2s\"/>" ;
-      
       else if (difficultyLevel == 'low' )
       speakOutput = speakOutput + String(i) + " <break time = \"4s\"/>" ;
-      
       else
       speakOutput = speakOutput + String(i) + " <break time = \"1s\"/>" ;
-          }
-//    const speechOutput = "So, I am gonna begin the count "+ count + "<break time = \"4s\"/>" + "hey" ;
-      const speechOutput = speakOutput;
-      
-      //creating home card
-      var cardTitle = 'Learn Counting!!';
-      var cardContent = "Hi! So, I will count till " + countnumber + ". Please follow along";
-      // var SimpleCard;
-      // SimpleCard.setTitle('Learn Counting me boii');
-      // SimpleCard.setContent('Learn Counting me boii content'); 
-      
+    }
+    const speechOutput = speakOutput;  
+    //setting variables for home card:
+    var cardTitle = 'Learn Counting!!';
+    var cardContent = "Hi! So, I will count till " + countnumber + ". Please follow along";
     return handlerInput.responseBuilder
       .speak(speechOutput)
       .withSimpleCard( cardTitle, cardContent)
-      // .SpeechletResponse.newTellResponse()
       .getResponse();
   },
 };
@@ -61,9 +44,8 @@ const HelpHandler = {
       && request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-     var cardTitle = 'Learn Counting!!';
-      var cardContent = 'You can say ask me to learn to count, or, you can say exit... What can I help you with?'
-    
+    var cardTitle = 'Learn Counting!!';
+    var cardContent = 'You can say ask me to learn to count, or, you can say exit... What can I help you with?'
     return handlerInput.responseBuilder
       .speak(HELP_MESSAGE)
       .reprompt(HELP_REPROMPT)
@@ -82,7 +64,6 @@ const ExitHandler = {
   handle(handlerInput) {
     var cardTitle = 'Bye!';
     var cardContent = "See you next time";
-     
     return handlerInput.responseBuilder
       .speak(STOP_MESSAGE)
       .withSimpleCard( cardTitle, cardContent)
@@ -125,11 +106,9 @@ const ErrorHandler = {
 };
 
 const SKILL_NAME = 'Learn Counting';
-// const GET_FACT_MESSAGE = 'Here\'s your fact: ';
 const HELP_MESSAGE = 'You can say ask me to learn to count, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
-
 
 const skillBuilder = Alexa.SkillBuilders.standard();
 
